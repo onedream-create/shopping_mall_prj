8579c607b7858acd7767c177cb1a9617c178a44e
user <user@DESKTOP-COJ9858>
2021-11-07 00:17:58 +0900

    selectLogin 수정

--- shopping_mall_prj/src/main/java/kr/co/shopping_mall/dao/AdminDAO.java
+++ shopping_mall_prj/src/main/java/kr/co/shopping_mall/dao/AdminDAO.java
@@ -1,22 +1,24 @@
 package kr.co.shopping_mall.dao;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import kr.co.shopping_mall.model.AdminVO;
 
 public class AdminDAO {
 	
-	public String selectLogin(AdminVO aVO) {
-		String admin_id = "";
-		//1.스프링 컨테이너 생성
+	public String selectLogin(AdminVO aVO) throws DataAccessException{
+		String admin_id=""; 
+		//1. Spring COntainer 생성
 		GetJdbcTemplate gjt = GetJdbcTemplate.getInstance();
 		//2.JdbcTemplate 얻기
 		JdbcTemplate jt = gjt.getJdbcTemplate();
 		//3. 쿼리문수행
-		String selectId="select admin_id from admin where admin_id=? and admin_pw=?";
-		admin_id = jt.queryForObject(selectId, new Object[] {aVO.getAdmin_id(),aVO.getAdmin_pw()},  String.class);
-		//4. 스프링컨테이너 닫기
+		String selectLogin="select admin_id from admin where admin_id=? and admin_pw=?";
+		admin_id = jt.queryForObject(selectLogin, new Object[] {aVO.getAdmin_id(),aVO.getAdmin_pw()},  String.class);
+		//4. Spring COntainer 닫기
 		gjt.closeAc();
+		
 		return admin_id;
 	}
 }
