c09eefe3d1e84f12a231b2534fa9eb8759fc1b29
user <user@DESKTOP-COJ9858>
2021-11-07 23:11:49 +0900

    비밀번호 변경 기능 완료

--- shopping_mall_prj/src/main/java/kr/co/shopping_mall/dao/AdminDAO.java
+++ shopping_mall_prj/src/main/java/kr/co/shopping_mall/dao/AdminDAO.java
@@ -3,8 +3,6 @@ package kr.co.shopping_mall.dao;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import kr.co.shopping_mall.model.AdminVO;
-
 public class AdminDAO {
 	
 	public String checkAccount(String inputId,String inputPw) throws DataAccessException{
@@ -14,11 +12,26 @@ public class AdminDAO {
 		//2.JdbcTemplate 얻기
 		JdbcTemplate jt = gjt.getJdbcTemplate();
 		//3. 쿼리문수행
-		String selectLogin="select admin_id from admin where admin_id=? and admin_pw=?";
-		admin_id = jt.queryForObject(selectLogin, new Object[] {inputId,inputPw},  String.class);
+		String selectId="select admin_id from admin where admin_id=? and admin_pw=?";
+		admin_id = jt.queryForObject(selectId, new Object[] {inputId,inputPw},  String.class);
 		//4. Spring COntainer 닫기
 		gjt.closeAc();
-		
 		return admin_id;
+	}
+	
+	public int changePass(String admin_id, String newPass) {
+		int cnt = 0;
+		
+		//스프링컨테이너 얻기
+		GetJdbcTemplate gjt=GetJdbcTemplate.getInstance();
+		//JdbcTemplate 얻기
+		JdbcTemplate jt=gjt.getJdbcTemplate();
+		//쿼리 실행
+		String updatePw="update admin set admin_pw=? where admin_id=?";
+		cnt=jt.update(updatePw,newPass,admin_id);
+		//스프링컨테이너 닫기
+		gjt.closeAc();
+		
+		return cnt;
 	}
 }
